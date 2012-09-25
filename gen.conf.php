b23a11ae9338c5b42b70635d5fd5e847605180db
Tony R Quilkey <thorpe@thorpesystems.com>
2012-09-26 07:44:03 +1000

    Add Gen config even though it uses defaults at this stage.

---
+++ gen.conf.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'extensions'    => 'extensions',
+    'content'       => 'content',
+    'templates'     => 'templates',
+    'assets'        => ['assets'],
+    'global'        => 'global.php',
+    'local'         => 'local.php',
+    'dest'          => 'build'
+];
